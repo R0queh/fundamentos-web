be30f6977d874833480ace184d3052b520fcd943
Bruno de Oliveira <brunosrdeoliveira@gmail.com>
2021-01-12 15:58:53 -0300

    adicionando proteção a erro de BIOS

--- html-aula/assets/js/script.js
+++ html-aula/assets/js/script.js
@@ -22,6 +22,7 @@ let emailOk =false
 let assuntoOk =false
 
 let mapa = document.querySelector('#mapa')
+let btn = document.querySelector('#btn')
 
 nome.style.width='100%'
 email.style.width='100%'
@@ -33,6 +34,7 @@ function validaNome(){
     if(nome.value.length < 3){
         txt.innerHTML = 'Nome inválido.'
         txt.style.color ='red'
+        nomeOk = false
     }else{
         txt.innerHTML = 'Nome válido.'
         txt.style.color ='green'
@@ -47,11 +49,13 @@ function validaEmail(){
     if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
         txtEmail.innerHTML = 'E-mail inválido'
         txtEmail.style.color = 'red'
+        emailOk=false
     }else{
         txtEmail.innerHTML = 'E-mail válido'
         txtEmail.style.color = 'green'
         emailOk =true
     }
+
 }
 
 
@@ -62,6 +66,7 @@ function validaAssunto(){
         txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 100 caractéres'
         txtAssunto.style.color= 'red'
         txtAssunto.style.display ='block'
+        assuntoOk=false
     }else{
         txtAssunto.style.display = 'none'
         assuntoOk =true
@@ -76,13 +81,21 @@ function enviar(){
     }
 }
 
+function ligarBtn(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true && assunto.value.length >3){
+        btn.style.visibility = 'visible'
+    }else{
+        btn.style.visibility = 'hidden'
+    }
+}
+
 
 function mapaZoom(){
     mapa.style.width = '800px'
-    mapa.style.heigth = '600px'
+    mapa.style.height = '600px'
 }
 
 function mapaNormal(){
     mapa.style.width = '400px'
-    mapa.style.heigth = '250px'
+    mapa.style.height = '250px'
 }
